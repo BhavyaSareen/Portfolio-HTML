d034afe9021426250faf9e0bfc91fbd9044b804a
BhavyaSareen <117308952+BhavyaSareen@users.noreply.github.com>
2023-12-23 18:59:03 +0530

    new

---
+++ form.js
@@ -0,0 +1,14 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 3,
+      autoHeight: true, // Auto height based on the content
+    autoplay: true,
+    autoWidth: true,
+      margin: 10,
+      loop: true
+      
+    });
+  });
+  
